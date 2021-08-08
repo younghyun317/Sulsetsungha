4571cf2c06d6f530dff9d94cf9820860c99b2405
younghyun <younghyun317@naver.com>
2021-08-09 01:58:15 +0900

    bottom navigation

---
+++ app/src/main/java/com/example/sulsetsungha/DonationFragment.java
@@ -0,0 +1,18 @@
+package com.example.sulsetsungha;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DonationFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_donation, container, false);
+    }
+}
--- app/src/main/java/com/example/sulsetsungha/MainActivity.java
+++ app/src/main/java/com/example/sulsetsungha/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.sulsetsungha;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout helpher;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        SettingListener();
+
+        //맨 처음 시작할 탭 설정
+        bottomNavigationView.setSelectedItemId(R.id.tab_home);
     }
+
+    private void init() {
+        helpher = findViewById(R.id.help_her);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+    }
+
+    private void SettingListener() {
+        //선택 리스너 등록
+        bottomNavigationView.setOnNavigationItemSelectedListener(new TabSelectedListener());
+    }
+
+    class TabSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.tab_home: {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.help_her, new HomeFragment())
+                            .commit();
+                    return true;
+                }
+                case R.id.tab_donation: {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.help_her, new DonationFragment())
+                            .commit();
+                    return true;
+                }
+                case R.id.tab_community: {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.help_her, new CommunityFragment())
+                            .commit();
+                    return true;
+                }
+                case R.id.tab_mypage: {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.help_her, new MypageFragment())
+                            .commit();
+                    return true;
+                }
+            }
+            return false;
+        }
+    }//TabSelectedListener
+
+
 }
