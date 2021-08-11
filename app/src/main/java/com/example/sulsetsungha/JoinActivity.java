7aa36794dc24a3e4d38f33b5ab9a37e9ec3c7314
younghyun <younghyun317@naver.com>
2021-08-11 23:54:11 +0900

    Make login and join Activity

---
+++ app/src/main/java/com/example/sulsetsungha/JoinActivity.java
@@ -0,0 +1,14 @@
+package com.example.sulsetsungha;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+}
