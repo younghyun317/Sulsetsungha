d95b8a6e372fa6ee2f4446495be1f22d5fa8f192
younghyun <younghyun317@naver.com>
2021-08-17 01:55:29 +0900

    design community write xml

---
+++ app/src/main/java/com/example/sulsetsungha/MyDonationListActivity.java
@@ -0,0 +1,14 @@
+package com.example.sulsetsungha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyDonationListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_donation_list);
+    }
+}
