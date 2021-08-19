ffb265cd3483dfa2c3988b3caaa56f0d9dc9809a
younghyun <younghyun317@naver.com>
2021-08-20 03:48:56 +0900

    borrow completed function

--- app/src/main/java/com/example/sulsetsungha/ChattingActivity.java
+++ app/src/main/java/com/example/sulsetsungha/ChattingActivity.java
@@ -14,12 +14,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.java_websocket.client.WebSocketClient;
@@ -32,6 +35,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ChattingActivity extends AppCompatActivity {
     String TAG = ChattingActivity.class.getSimpleName();
@@ -41,6 +45,7 @@ public class ChattingActivity extends AppCompatActivity {
     WebSocketClient webSocketClient;
     EditText chat;
     Button send;
+    ImageButton btnBorrowCompleted;
 
     RecyclerView chat_message;
     ArrayList<ChatItem> chatItems = new ArrayList<>();
@@ -50,8 +55,6 @@ public class ChattingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatting);
-
-
 
         connectWebSocket();
 
@@ -77,6 +80,64 @@ public class ChattingActivity extends AppCompatActivity {
             }
         });
 
+        btnBorrowCompleted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setBorrowCompleted();
+            }
+        });
+
+    }
+
+    private void setBorrowCompleted() {
+        Intent intent = getIntent();
+        String user_name = intent.getStringExtra("username");
+
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final String url = "http://3.38.51.117:8000/borrow/";
+
+        HashMap<String, String> borrow_json = new HashMap<>();
+        borrow_json.put("borrower_username", user_name.toString());
+        JSONObject parameter = new JSONObject(borrow_json);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String token = sharedPreferences.getString("access_token", null);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                url,
+                parameter,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast toast = Toast.makeText(getApplicationContext(), "대여가 완료되었습니다.", Toast.LENGTH_LONG);
+                        toast.show();
+
+                        finish();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                return give_token(token);
+            }
+        };
+
+        queue.add(jsonObjectRequest);
+
+    }
+
+    Map<String, String> give_token(String token) {
+        HashMap<String, String> headers = new HashMap<>();
+        // Bearer + token 해야됨! 안그럼 인식 못함
+        headers.put("Authorization", "Bearer " + token);
+
+        return headers;
     }
 
     void connectWebSocket() {
--- app/src/main/java/com/example/sulsetsungha/Fragment/MypageAdapter.java
+++ app/src/main/java/com/example/sulsetsungha/Fragment/MypageAdapter.java
@@ -20,6 +20,9 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -40,88 +43,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-class MypageAdapter extends ArrayAdapter implements AdapterView.OnItemClickListener {
-
-    String TAG = DonationAdapter.class.getSimpleName();
-
-    private Context context;
-    private List list;
-    private LayoutInflater layoutInflater;
-    private ProgressBar prgbarDonation;
-    private int value;
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(context, "clicked", Toast.LENGTH_SHORT).show();
-    }
-
-    class ViewHolder {
-        public TextView txt_company;
-        public TextView txt_title;
-        public ProgressBar prg_donation;
-        public TextView txt_dday;
-        public TextView txt_donation;
-    }
-
-    public MypageAdapter(Context context, ArrayList list){
-        super(context, 0, list);
-        this.context = context;
-        this.layoutInflater = LayoutInflater.from(context);
-        this.list = list;
-    }
-
-    //리스트뷰에서 아이템을 하나씩 가져오는 함수
-    //position : 아이템의 index, convertView: index에 해당되는 view 객체, parent: view 객체가 포함된 부모
-    public View getView(int position, View convertView, ViewGroup parent) {
-        final MypageAdapter.ViewHolder viewHolder;
-        viewHolder = new MypageAdapter.ViewHolder();
-
-        if (convertView == null){
-            LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-            convertView = layoutInflater.inflate(R.layout.item_donation, parent, false);
-        }
-
-        View bodyView = (View)convertView.findViewById(R.id.body);
-
-        viewHolder.txt_company = (TextView)convertView.findViewById(R.id.txtName);
-        viewHolder.txt_title = (TextView)convertView.findViewById(R.id.txtSummary);
-        viewHolder.prg_donation = (ProgressBar)convertView.findViewById(R.id.prgbarDonation);
-        viewHolder.txt_dday = (TextView)convertView.findViewById(R.id.txtDday);
-        viewHolder.txt_donation = (TextView)convertView.findViewById(R.id.txtDonation);
-
-        final MypageFragment.Sponsor sponsor = (MypageFragment.Sponsor)list.get(position);
-        viewHolder.txt_company.setText(sponsor.getCompany().toString());
-        viewHolder.txt_title.setText(sponsor.getTitle().toString());
-        viewHolder.txt_dday.setText(sponsor.getDday().toString());
-        viewHolder.txt_donation.setText(sponsor.getDonation().toString());
-        viewHolder.prg_donation.setProgress((int) Math.round(Double.parseDouble(sponsor.getDonation())));
-
-        bodyView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, DonationDetailActivity.class);
-                intent.putExtra("Company", sponsor.getCompany());
-                intent.putExtra("Title", sponsor.getTitle().toString());
-                intent.putExtra("Context", sponsor.getContext().toString());
-                intent.putExtra("Dday", sponsor.getDday().toString());
-                intent.putExtra("Percent", sponsor.getDonation().toString());
-                intent.putExtra("Donation", sponsor.getDonation());
-
-                context.startActivity(intent);
-
-            }
-        });
-
-
-        convertView.setTag(viewHolder);
-        return convertView;
-    }
-
-    Map <String, String> give_token(String token) {
-        HashMap<String, String> headers = new HashMap<>();
-        headers.put("Authorization", "Bearer " + token);
-
-        return headers;
-    }
+public class MypageAdapter  {
 
 }
