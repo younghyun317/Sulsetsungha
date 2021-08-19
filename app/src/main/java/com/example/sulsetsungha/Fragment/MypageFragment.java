8164db3ecf6a8a0a6f9b4858950c3cf5e2b8697c
younghyun <younghyun317@naver.com>
2021-08-20 03:01:21 +0900

    design mypage mydonation list

--- app/src/main/java/com/example/sulsetsungha/Fragment/MypageFragment.java
+++ app/src/main/java/com/example/sulsetsungha/Fragment/MypageFragment.java
@@ -8,8 +8,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,17 +30,31 @@ import com.example.sulsetsungha.R;
 import com.example.sulsetsungha.community.CommunityDetailActivity;
 import com.example.sulsetsungha.community.CommunityWriteActivity;
 import com.example.sulsetsungha.community.MyPostActivity;
+import com.example.sulsetsungha.donation.DonationAdapter;
+import com.example.sulsetsungha.donation.DonationFragment;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MypageFragment extends Fragment{
 
     String TAG = MypageFragment.class.getSimpleName();
+
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+    SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+    String today = dateFormat.format(Calendar.getInstance().getTime());
+    ArrayList<MypageFragment.Sponsor> sponsors;
+    ListView mypageListView;
+    private static MypageAdapter mypageAdapter;
 
     String ID;
 
@@ -85,6 +101,96 @@ public class MypageFragment extends Fragment{
 //                startActivity(i);
             }
         });
+
+        //기부 내역 불러오기
+        final RequestQueue queue = Volley.newRequestQueue(getContext());
+        final String url = "http://3.38.51.117:8000/donation_user/";
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String token = sharedPreferences.getString("access_token", null);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, "response: " + response);
+                        try {
+                            //후원 리스트
+                            sponsors = new ArrayList<>();
+                            //Log.d("response", "response : " + response.getJSONObject(0).getString("company").toString());
+                            //Log.d("length", "length : " + response.getJSONArray(1).toString());
+
+                            String id, company, title, context, deadline;
+                            long dday, target_amount, current_amount;
+                            double percent;
+
+                            //int percent;
+                            Date currentCal, targetCal; //현재 날짜, 비교 날짜
+
+                            for (int i=0; i < response.length(); i++) {
+                                id = response.getJSONObject(i).getString("id").toString();
+                                company = response.getJSONObject(i).getString("company").toString();
+                                title = response.getJSONObject(i).getString("title").toString();
+                                context = response.getJSONObject(i).getString("context").toString();
+                                deadline = response.getJSONObject(i).getString("deadline").toString();
+                                target_amount = Long.parseLong(response.getJSONObject(i).getString("target_amount"));
+                                current_amount = Long.parseLong(response.getJSONObject(i).getString("current_amount"));
+
+                                //percent = Math.round((current_amount/target_amount)*100);
+                                percent = ((current_amount * 1.0)/target_amount)*100;
+                                Log.d(TAG, "current_amount : " + String.valueOf(current_amount).toString());
+                                Log.d(TAG, "target_amount : " + String.valueOf(target_amount).toString());
+                                Log.d(TAG, "percent : " + String.valueOf(percent).toString());
+                                currentCal = dateFormat.parse(today);
+                                targetCal = dateFormat.parse(deadline);
+
+                                // Date로 변환된 두 날짜를 계산한 뒤 그 리턴값으로 long type 변수를 초기화 하고 있다.
+                                // 연산결과 -950400000. long type 으로 return 된다.
+                                long calDate = targetCal.getTime() - currentCal.getTime();
+                                // Date.getTime() 은 해당날짜를 기준으로1970년 00:00:00 부터 몇 초가 흘렀는지를 반환해준다.
+                                // 이제 24*60*60*1000(각 시간값에 따른 차이점) 을 나눠주면 일수가 나온다.
+                                long calDateDays = calDate / ( 24*60*60*1000);
+
+                                dday = Math.abs(calDateDays);
+
+                                sponsors.add(new Sponsor(id, company, title, context, Long.toString(dday), Double.toString(percent)));
+                            }
+
+                            mypageListView = (ListView)view.findViewById(R.id.listView_MyDonation);
+                            mypageAdapter = new MypageAdapter(getContext(), sponsors);
+                            mypageListView.setAdapter(mypageAdapter);
+
+                            mypageAdapter.notifyDataSetChanged();//갱신하기
+
+                            mypageListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    //각 아이템을 분간 할 수 있는 position과 뷰
+                                    String selectedItem = (String) view.findViewById(R.id.txtName).getTag().toString();
+                                    Toast.makeText(getContext(), "Clicked: " + position +" " + selectedItem, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        } catch (JSONException | ParseException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                return give_token(token);
+            }
+        };
+
+        queue.add(jsonArrayRequest);
 
         return view;
     }
@@ -201,40 +307,6 @@ public class MypageFragment extends Fragment{
 
     }
 
-    private void getUserDonation() {
-        final RequestQueue queue = Volley.newRequestQueue(getContext());
-        final String url = "http://3.38.51.117:8000/donation_user/";
-
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
-        String token = sharedPreferences.getString("access_token", null);
-
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
-                url,
-                null,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        Log.d(TAG, "response get lend : " + response.length());
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                })
-        {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                return give_token(token);
-            }
-        };
-
-        queue.add(jsonArrayRequest);
-
-
-    }
-
     Map<String, String> give_token(String token) {
         HashMap<String, String> headers = new HashMap<>();
         // Bearer + token 해야됨! 안그럼 인식 못함
@@ -243,4 +315,49 @@ public class MypageFragment extends Fragment{
         return headers;
     }
 
+    class Sponsor {
+        private String id;
+        private String company;
+        private String title;
+        private String context;
+        private String dday;
+        private String donation;
+        private int progressbar;
+        //private ProgressBar progressbar;
+
+        public Sponsor(String id, String company, String title, String context, String dday, String donation) {
+            //this.request = request;
+            this.id = id;
+            this.company = company;
+            this.title = title;
+            this.context = context;
+            this.dday = dday;
+            this.donation = donation;
+            //this.progressbar = progressbar;
+        }
+
+        public String getId() { return id; }
+
+        public String getCompany() {
+            return company;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getContext() { return context; }
+
+        public String getDday() {
+            return dday;
+        }
+
+        public String getDonation() {
+            return donation;
+        }
+
+        public int getProgressbar() {
+            return progressbar;
+        }
+    } //class Sponsor
 }
