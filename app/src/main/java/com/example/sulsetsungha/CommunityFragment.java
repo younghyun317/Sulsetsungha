032cc000b012acc3158a200a2538904cc55e4e97
younghyun <younghyun317@naver.com>
2021-08-19 16:50:06 +0900

    modify community

--- app/src/main/java/com/example/sulsetsungha/CommunityFragment.java
+++ app/src/main/java/com/example/sulsetsungha/CommunityFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.LayoutInflater;
